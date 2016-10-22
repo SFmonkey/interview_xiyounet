1ad4f0b9abd2581df93312141b43da62c6ef4dda
SFmonkey <786134878@qq>
2016-10-22 20:29:51 +0800

    解决API数据格式

--- js/written_test_interaction.js
+++ js/written_test_interaction.js
@@ -23,48 +23,66 @@ $(function(){
             return {};
         }
     }
-    var item =[[4,2,2],['填空题','为了提高数据库的性能，需要针对系统设计基准测试进行压力测试，那么进行压力测试时需要考虑什么指标',1],
-     ['填空题','A，B，C三位同学都是很聪明的同学，面试官给他们背上依次贴上了数字2，4，8，他们都能看见别人的数字但无法看见自己的数字，现在面试官告诉他们这些数字都是自然数并且构成一个等比数列，让A、B、C同学依次循环回答是否确定自己的数字是多少，每位同学的回答算作一次，经过____次有同学能准确说出自己的数字',2],
-     ['填空题','给定一个整数sum，从有N个无序元素的数组中寻找元素a、b、c、d，使得 a+b+c+d =sum，最快的平均时间复杂度是____。',2],
-     ['填空题','已知int a[]={1,2,3,4,5}；int*p[]={a,a+1,a+2,a+3}；int **q=p；表达式*(p[0]+1)+**(q+2)的值是____。',3],
-     ['简答题','设二叉树结点的先根序列、中根序列和后根序列中，所有叶子结点的先后顺序',2],
-     ['简答题','有8只球队,采用抽签的方式随机配对,组成4场比赛。假设其中有4只强队,那么出现强强对话 (任意两只强队相遇)的概率',3],
-     ['编程题','给定一个长度为n的整数数组a，元素均不相同，问数组是否存在这样一个片段，只将该片段翻转就可以使整个数组升序排列。其中数组片段[l,r]表示序列a[l], a[l+1], ..., a[r]。原始数组为a[1], a[2], ..., a[l-2], a[l-1], a[l], a[l+1], ..., a[r-1], a[r], a[r+1], a[r+2], ..., a[n-1], a[n]，将片段[l,r]反序后的数组是a[1], a[2], ..., a[l-2], a[l-1], a[r], a[r-1], ..., a[l+1], a[l], a[r+1], a[r+2], ..., a[n-1], a[n]。',2],
-     ['编程题','V先生有一天工作到很晚，回家的时候要穿过一条长l的笔直的街道，这条街道上有n个路灯。假设这条街起点为0，终点为l，第i个路灯坐标为ai。路灯发光能力以正数d来衡量，其中d表示路灯能够照亮的街道上的点与路灯的最远距离，所有路灯发光能力相同。为了让V先生看清回家的路，路灯必须照亮整条街道，又为了节省电力希望找到最小的d是多少？',3]];
-    for(var i=0;i<item[0][0]+item[0][1];i++){
-        $('#main_test_content_title_0').append('<h2 id="main_test_content_title_name_'+i+'">'+(i+1)+'.'+item[i+1][0]+'</h2>');
-        $('#main_test_content_0').append('<p id="main_test_content_item_'+i+'">'+item[i+1][1]+'</p>');
-        $('#main_test_content_content_0').append('<textarea class="main_test_content_explain" id="main_test_content_explain_'+i+'" placeholder="写出你的答案和思路٩(◦`꒳´◦)۶"></textarea>');
-        $('#main_menu_0').append('<p id="main_menu_grade_'+i+'"><span>试题难度 :</span> <span class="stnandu"></span></p>');
-        $('#main_menu_item_list_0').append('<span id="main_menu_item_list_id_0">'+(i+1)+'</span>');
-    }
-    $('main_menu_item_list_id_0').addClass('item_2');
-    for(var i=0;i<item[0][2];i++){
-        $('#main_test_content_title_1').append('<h2 id="main_test_content_title_name_1_'+i+'">'+(i+1)+'.'+item[i+7][0]+'</h2>');
-        $('#main_test_content_1').append('<p id="main_test_content_item_1_'+i+'">'+item[i+7][1]+'</p>');
-        $('#main_test_content_content_1').append('<textarea class="main_test_content_explain" id="main_test_content_explain_1_'+i+'" placeholder="写出你的答案和思路٩(◦`꒳´◦)۶"></textarea>');
-        $('#main_menu_1').append('<p id="main_menu_grade_1_'+i+'"><span>试题难度 :</span> <span class="stnandu"></span></p>');
-        $('#main_menu_item_list_1').append('<span id="main_menu_item_list_id_0">'+(i+1)+'</span>');
-    }
-    $('main_menu_item_list_id_1').addClass('item_2');
-    for(var i=0;i<item[0][0]+item[0][1];i++){
-        for(var j=0;j<item[i+1][2];j++){
-            $('#main_menu_grade_'+i+' .stnandu').append('<img src="images/star-on.png" alt="grade">');
+
+    $.ajax({
+        url: 'demo_item.php',
+        type: 'GET',
+        cache: false,
+        async: true,
+        dataType: 'json',
+        success: function (data) {
+            transfer(data);
         }
-    }
-    for(var i=0;i<item[0][2];i++){
-        for(var j=0;j<item[i+7][2];j++){
-            $('#main_menu_grade_1_'+i+' .stnandu').append('<img src="images/star-on.png" alt="grade">');
+    });
+    var transfer = function (item) {
+        for (var i = 0; i < item.type[0]; i++) {
+            $('#main_test_content_title_0').append('<h2 id="main_test_content_title_name_' + i + '">' + (i + 1) + '.' + item.tiankong[i][0] + '</h2>');
+            $('#main_test_content_0').append('<p id="main_test_content_item_' + i + '">' + item.tiankong[i][1] + '</p>');
+            $('#main_test_content_content_0').append('<textarea class="main_test_content_explain" id="main_test_content_explain_' + i + '" placeholder="写出你的答案和思路٩(◦`꒳´◦)۶"></textarea>');
+            $('#main_menu_0').append('<p id="main_menu_grade_' + i + '"><span>试题难度 :</span> <span class="stnandu"></span></p>');
+            $('#main_menu_item_list_0').append('<span id="main_menu_item_list_id_0">' + (i + 1) + '</span>');
         }
-    }
-    $('#item_2').hide();
-    $('#aside_2').hide();
-    for(var i=1;i<6;i++){
-        $('#main_test_content_title_name_'+i+'').hide();
-        $('#main_test_content_item_'+i+'').hide();
-        $('#main_test_content_explain_'+i+'').hide();
-        $('#main_menu_grade_'+i+'').hide();
-    }
+        for (var i = 0; i < item.type[1]; i++) {
+            $('#main_test_content_title_0').append('<h2 id="main_test_content_title_name_' + (i + item.type[0]) + '">' + (i + item.type[0] + 1) + '.' + item.jianda[i][0] + '</h2>');
+            $('#main_test_content_0').append('<p id="main_test_content_item_' + (i + item.type[0]) + '">' + item.jianda[i][1] + '</p>');
+            $('#main_test_content_content_0').append('<textarea class="main_test_content_explain" id="main_test_content_explain_' + (i + item.type[0]) + '" placeholder="写出你的答案和思路٩(◦`꒳´◦)۶"></textarea>');
+            $('#main_menu_0').append('<p id="main_menu_grade_' + (i + item.type[0]) + '"><span>试题难度 :</span> <span class="stnandu"></span></p>');
+            $('#main_menu_item_list_0').append('<span id="main_menu_item_list_id_0">' + (i + 1) + '</span>');
+        }
+        $('main_menu_item_list_id_0').addClass('item_2');
+        for (var i = 0; i < item.type[1]; i++) {
+            $('#main_test_content_title_1').append('<h2 id="main_test_content_title_name_1_' + i + '">' + (i + 1) + '.' + item.biancheng[i][0] + '</h2>');
+            $('#main_test_content_1').append('<p id="main_test_content_item_1_' + i + '">' + item.biancheng[i][1] + '</p>');
+            $('#main_test_content_content_1').append('<textarea class="main_test_content_explain" id="main_test_content_explain_1_' + i + '" placeholder="写出你的答案和思路٩(◦`꒳´◦)۶"></textarea>');
+            $('#main_menu_1').append('<p id="main_menu_grade_1_' + i + '"><span>试题难度 :</span> <span class="stnandu"></span></p>');
+            $('#main_menu_item_list_1').append('<span id="main_menu_item_list_id_0">' + (i + 1) + '</span>');
+        }
+        $('main_menu_item_list_id_1').addClass('item_2');
+        for (var i = 0; i < item.type[0]; i++) {
+            for (var j = 0; j < item.tiankong[i][2]; j++) {
+                $('#main_menu_grade_' + i + ' .stnandu').append('<img src="images/star-on.png" alt="grade">');
+            }
+        }
+        for (var i = 0; i < item.type[1]; i++) {
+            for (var j = 0; j < item.jianda[i][2]; j++) {
+                $('#main_menu_grade_' + (i + item.type[0]) + ' .stnandu').append('<img src="images/star-on.png" alt="grade">');
+                console.log(i + item.type[0]);
+            }
+        }
+        for (var i = 0; i < item.type[2]; i++) {
+            for (var j = 0; j < item.biancheng[i][2]; j++) {
+                $('#main_menu_grade_1_' + i + ' .stnandu').append('<img src="images/star-on.png" alt="grade">');
+            }
+        }
+        $('#item_2').hide();
+        $('#aside_2').hide();
+        for (var i = 1; i < 6; i++) {
+            $('#main_test_content_title_name_' + i + '').hide();
+            $('#main_test_content_item_' + i + '').hide();
+            $('#main_test_content_explain_' + i + '').hide();
+            $('#main_menu_grade_' + i + '').hide();
+        }
+    };
     //submit
     $('#submit_1').click(function(){
         var result = confirm("你确定提交第一部分吗？");
